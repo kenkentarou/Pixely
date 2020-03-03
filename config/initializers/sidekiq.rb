050fc185d3ad489ac6bd2341efb3b385b157db87
Taro <zettaigoukaku547@gmail.com>
2020-03-03 21:00:31 +0900

    修正

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,25 @@
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = {
+        url: 'redis://localhost:6379'
+    }
+  end
 
-Sidekiq.configure_server do |config|
-  config.redis = {
-      url: 'redis://h:p0ecdc881139158ca85b95c91613d216c51468f002eb225f2bba4d9533c12dd4e@ec2-34-203-164-221.compute-1.amazonaws.com:17129'
-  }
-end
+  Sidekiq.configure_client do |config|
+    config.redis = {
+        url: 'redis://localhost:6379'
+    }
+  end
+else
+  Sidekiq.configure_server do |config|
+    config.redis = {
+        url: 'redis://h:p0ecdc881139158ca85b95c91613d216c51468f002eb225f2bba4d9533c12dd4e@ec2-34-203-164-221.compute-1.amazonaws.com:17129'
+    }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = {
-      url: 'redis://h:p0ecdc881139158ca85b95c91613d216c51468f002eb225f2bba4d9533c12dd4e@ec2-34-203-164-221.compute-1.amazonaws.com:17129'
-  }
+  Sidekiq.configure_client do |config|
+    config.redis = {
+        url: 'redis://h:p0ecdc881139158ca85b95c91613d216c51468f002eb225f2bba4d9533c12dd4e@ec2-34-203-164-221.compute-1.amazonaws.com:17129'
+    }
+  end
 end
